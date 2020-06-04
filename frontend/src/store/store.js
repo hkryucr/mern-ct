68fceb4258a8233a742e7b1b7f35879b1a46515b
Henry Ryu <hkryucr@gmail.com>
2020-06-04 00:47:28 -0700

    commented out the logger

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,15 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import appReducer from '../reducers/app_reducer';
 
-const configureStore = (preloadedState = {}) => (
-    createStore(
-        appReducer,
-        preloadedState,
-        applyMiddleware(thunk, logger)
-    )
+const configureStore = (preloadedState = {}) =>
+  createStore(
+    appReducer,
+    preloadedState,
+    applyMiddleware(thunk)
 );
+    // applyMiddleware(thunk, logger)
 
 export default configureStore;
